363ccd708db2705fc13166f8d447ad24b54443c2
quincinia <eanj13@gmail.com>
2022-05-17 18:31:55 -0700

    Challonge tourney can be parsed

--- requests/challonge/challonge.go
+++ requests/challonge/challonge.go
@@ -1,6 +1,11 @@
 // Package challonge contains functions for handling Challonge-based tournaments.
 package challonge
 
+// The JSON response puts a wrapper around the tournament object.
+type Challonge struct {
+	Tourney Tournament `json:"tournament"`
+}
+
 // Similar to models.Tournament, however only the fields available in the Challonge response are included.
 type Tournament struct {
 	Name           string
@@ -8,6 +13,8 @@ type Tournament struct {
 	NumEntrants    int    `json:"participants_count"`
 	UniquePlacings int    `json:"-"`
 	BracketReset   bool   `json:"-"`
+	Participants   []Attendee
+	Matches        []Match
 }
 
 // Similar to models.Attendee, but only containing relevant fields.
@@ -22,4 +29,5 @@ type Match struct {
 	Player2ID int `json:"player2_id"`
 	WinnerID  int `json:"winner_id"`
 	LoserID   int `json:"loser_id"`
+	Round     int
 }
--- requests/challonge/challonge_test.go
+++ requests/challonge/challonge_test.go
@@ -1,1 +1,34 @@
 package challonge
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestDecoder(t *testing.T) {
+	response, err := os.Open("challonge.json")
+	if err != nil {
+		t.Error(err)
+	}
+	defer response.Close()
+
+	decoder := json.NewDecoder(response)
+	var c Challonge
+	err = decoder.Decode(&c)
+	if err != nil {
+		t.Error(err)
+	}
+
+	tourney := c.Tourney
+	fmt.Println(tourney.Name, tourney.URL, tourney.NumEntrants)
+
+	if len(tourney.Participants) != 20 {
+		t.Error("Incorrect number of participants", len(tourney.Participants))
+	}
+
+	if len(tourney.Matches) != 39 {
+		t.Error("Incorrect number of matches", len(tourney.Matches))
+	}
+}
