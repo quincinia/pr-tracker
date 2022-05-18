0cf9809db66100813b698caab71566c48c8ba591
quincinia <eanj13@gmail.com>
2022-05-18 14:51:34 -0700

    Can now calculate unique placings

--- requests/challonge/challonge.go
+++ requests/challonge/challonge.go
@@ -31,3 +31,36 @@ type Match struct {
 	LoserID   int `json:"loser_id"`
 	Round     int
 }
+
+// In JavaScript: Math.trunc(Math.log2(n))
+// Do not use with negative numbers.
+func TruncLog2(n int) (exp, value int) {
+	if n <= 0 {
+		return
+	}
+	value = 1
+	n = n >> 1
+	for n != 0 {
+		exp++
+		value *= 2
+		n = n >> 1
+	}
+	return
+}
+
+func CalculatePlacings(numEntrants int) (placings int) {
+	// Annoying edge cases
+	if numEntrants < 1 {
+		return -1
+	}
+	if numEntrants <= 4 {
+		return numEntrants
+	}
+	
+	exp, value := TruncLog2(numEntrants)
+	placings = 2*exp + 1
+	if numEntrants > (3*value)/2 {
+		placings++
+	}
+	return
+}
--- requests/challonge/challonge_test.go
+++ requests/challonge/challonge_test.go
@@ -32,3 +32,45 @@ func TestDecoder(t *testing.T) {
 		t.Error("Incorrect number of matches", len(tourney.Matches))
 	}
 }
+
+func TestTrunc(t *testing.T) {
+	var n, exp, value int
+
+	t.Run("Negative n", func(t *testing.T) {
+		n = -1;
+		_, value = TruncLog2(n)
+		if value != 0 {
+			t.Error("got", value, ", want 0")
+		}
+	})
+
+	t.Run("Power of 2", func(t *testing.T) {
+		n = 16
+		exp, value = TruncLog2(n)
+		if exp != 4 || value != 16 {
+			t.Error("got", exp, ", want 4")
+			t.Error("got", value, ", want 16")
+
+		}
+	})
+
+	t.Run("Between powers", func(t *testing.T) {
+		n = 31
+		exp, value = TruncLog2(n)
+		if exp != 4 || value != 16 {
+			t.Error("got", exp, ", want 4")
+			t.Error("got", value, ", want 16")
+
+		}
+	})
+}
+
+func TestCalcPlacings(t *testing.T) {
+	tests := []int{1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97}
+	for index, test := range tests {
+		placings := CalculatePlacings(test)
+		if placings != index+1 {
+			t.Error("got", placings, "want", index+1)
+		}
+	}
+}
